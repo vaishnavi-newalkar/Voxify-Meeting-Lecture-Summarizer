b148d2bb58eeb668f3ada61652e5a4a885c57fff
GitNinja11 <vaishnavinewalkar11@gmail.com>
2026-06-09 20:26:00 +0530

    fix Vite env loading1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,6 +13,7 @@
 //     setupFiles: ['./src/__tests__/setup.js'],
 //   },
 // });
+/* eslint-env node */
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
