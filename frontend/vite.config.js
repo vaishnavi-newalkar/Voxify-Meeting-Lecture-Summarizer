f43e5b7727d5a02874b13674866dd2b8dc9ccbe6
GitNinja11 <vaishnavinewalkar11@gmail.com>
2026-06-09 22:04:43 +0530

    fixing lint error1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@
 //     setupFiles: ['./src/__tests__/setup.js'],
 //   },
 // });
-/* eslint-env node */
+/* global process */
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
